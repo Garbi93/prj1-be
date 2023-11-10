e885a8afe1c103b16c369e088aa1317ab2fa8b9c
Garbi93 <iseung2093@gmail.com>
2023-11-10 10:33:22 +0900

    update를 db에 저장 하는 back

--- src/main/java/com/example/prj1be/controller/BoardController.java
+++ src/main/java/com/example/prj1be/controller/BoardController.java
@@ -48,4 +48,10 @@ public class BoardController {
             return ResponseEntity.internalServerError().build();
         }
     }
+
+    @PutMapping("edit")
+    public void edit(@RequestBody Board board) {
+//        System.out.println("board = " + board);
+        service.update(board);
+    }
 }
