b9a26fdbcd3b592314b7607a4d6ab6e856f77e39
Garbi93 <iseung2093@gmail.com>
2023-11-14 09:45:18 +0900

    11/14 로그인 후 Member객체 session attribute "login" 추가

--- src/main/java/com/example/prj1be/controller/MemberController.java
+++ src/main/java/com/example/prj1be/controller/MemberController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
 
 import java.util.List;
 
@@ -96,9 +97,9 @@ public class MemberController {
     }
 
     @PostMapping("login")
-    public ResponseEntity login(@RequestBody Member member){
+    public ResponseEntity login(@RequestBody Member member, WebRequest request){
 
-        if(service.login(member)){
+        if(service.login(member, request)){
             return ResponseEntity.ok().build();
         }else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
