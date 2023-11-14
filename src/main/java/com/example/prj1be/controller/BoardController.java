cbf617e5b7a41775e254d686ec04c8ec6c840960
Garbi93 <iseung2093@gmail.com>
2023-11-14 10:29:21 +0900

    11/14 글 작성시 로그인한 사용자의 id가 writer열에 들어감

--- src/main/java/com/example/prj1be/controller/BoardController.java
+++ src/main/java/com/example/prj1be/controller/BoardController.java
@@ -4,6 +4,8 @@ import com.example.prj1be.domain.Board;
 import com.example.prj1be.domain.Member;
 import com.example.prj1be.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.logging.Log;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -21,14 +23,16 @@ public class BoardController {
     public ResponseEntity add(@RequestBody Board board,
                               @SessionAttribute(value = "login", required = false) Member login) {
 
-        System.out.println("login = " + login);
+        if(login == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
 
         if (!service.validate(board)){
             return ResponseEntity.badRequest().build();
         }
 
 
-        if(service.save(board)) {
+        if(service.save(board, login)) {
             return ResponseEntity.ok().build();
         }else {
             return ResponseEntity.internalServerError().build();
