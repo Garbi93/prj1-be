2cd1f7df103020560d1f1c517a8abd29a865a391
Garbi93 <iseung2093@gmail.com>
2023-11-17 12:17:40 +0900

    11/17 좋아요 있는 게시물 삭제 좋아요 한 회원 탈퇴

--- src/main/java/com/example/prj1be/service/MemberService.java
+++ src/main/java/com/example/prj1be/service/MemberService.java
@@ -4,6 +4,7 @@ import com.example.prj1be.domain.Auth;
 import com.example.prj1be.domain.Member;
 import com.example.prj1be.mapper.BoardMapper;
 import com.example.prj1be.mapper.CommentMapper;
+import com.example.prj1be.mapper.LikeMapper;
 import com.example.prj1be.mapper.MemberMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,6 +21,7 @@ public class MemberService {
     private final BoardMapper boardMapper;
     private final CommentMapper commentMapper;
     private final BoardService boardService;
+    private final LikeMapper likeMapper;
 
     public boolean add(Member member) {
         return mapper.insert(member) == 1;
@@ -73,8 +75,11 @@ public class MemberService {
         // 1-2 게시물 번호들 loop 각 게시물 삭제 (boardService.remove)
         boardIdList.forEach((boardId) -> boardService.remove(boardId));
 
+        // 2-1 이 맴버가 좋아요 한것
+        likeMapper.deleteByMemberId(id);
 
-        // 2. 이 멤버 삭제
+
+        // 3. 이 멤버 삭제
         return mapper.deleteById(id) == 1;
     }
 
